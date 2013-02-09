1361d6cf4f6171b8ec3815d084c5956fe05ed762
Rudy <rodrix2000@ubuntu.(none)>
2013-02-09 14:40:39 -0600

    install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = 'ea21d3fb3d606db7558461f3373ffc7b95f5540d61a6cb0efe7be7c1f56bdd0b1c64434211cbcfcbaeef5b572967b9988d5831673094112e2123a90359c62a93'
